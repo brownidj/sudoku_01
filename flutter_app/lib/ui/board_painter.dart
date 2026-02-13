74d47e20302cf66247ba5a828a6357bb69ff8567
David Browning <david.browning@me.com>
2026-02-13 11:21:46 +1000

    Corrected Numbers mode Notes

--- flutter_app/lib/ui/board_painter.dart
+++ flutter_app/lib/ui/board_painter.dart
@@ -145,6 +145,10 @@ class SudokuBoardPainter extends CustomPainter {
     }
     final gridSize = noteGridSize(notesSorted.length);
     final subCellSize = rect.width / gridSize;
+    if (state.contentMode == 'numbers') {
+      _drawNumberNotes(canvas, rect, notesSorted, gridSize, subCellSize);
+      return;
+    }
     final logicalSize = subCellSize * 0.95;
     final targetPx = logicalSize * devicePixelRatio;
     final sizePx = bestNoteSize(targetPx, noteImagesBySize.keys);
@@ -170,6 +174,47 @@ class SudokuBoardPainter extends CustomPainter {
       final target = Rect.fromLTWH(left, top, logicalSize, logicalSize);
       paintImage(canvas: canvas, rect: target, image: image, fit: BoxFit.contain);
     }
+  }
+
+  void _drawNumberNotes(
+    Canvas canvas,
+    Rect rect,
+    List<int> notesSorted,
+    int gridSize,
+    double subCellSize,
+  ) {
+    final maxNotes = gridSize * gridSize;
+    for (var i = 0; i < notesSorted.length && i < maxNotes; i += 1) {
+      final digit = notesSorted[i];
+      final row = i ~/ gridSize;
+      final col = i % gridSize;
+      final cellLeft = rect.left + col * subCellSize;
+      final cellTop = rect.top + row * subCellSize;
+      final cellRect = Rect.fromLTWH(cellLeft, cellTop, subCellSize, subCellSize);
+      _drawNoteDigit(canvas, cellRect, digit);
+    }
+  }
+
+  void _drawNoteDigit(Canvas canvas, Rect rect, int digit) {
+    final fontSize = rect.width * 0.6;
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: digit.toString(),
+        style: TextStyle(
+          color: style.valueColor.withOpacity(0.7),
+          fontWeight: FontWeight.w500,
+          fontSize: fontSize,
+        ),
+      ),
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+    )..layout(minWidth: 0, maxWidth: rect.width);
+
+    final offset = Offset(
+      rect.left + (rect.width - textPainter.width) / 2,
+      rect.top + (rect.height - textPainter.height) / 2,
+    );
+    textPainter.paint(canvas, offset);
   }
 
 
